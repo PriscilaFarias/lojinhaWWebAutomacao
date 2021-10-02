58a3fee5f43d856c1264754481a06785ffb17adb
priscilafarias <priscila.abfarias@gmail.com>
2021-10-01 22:08:22 -0300

    primeiro commit

---
+++ src/test/java/modulos/produtos/ProdutosTest.java
@@ -0,0 +1,55 @@
+package modulos.produtos;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+import java.time.Duration;
+
+@DisplayName("Testes Web do Modulo de Produtos")
+public class ProdutosTest {
+    @Test
+    @DisplayName("Não é permetido registrar um produto com valor igual a zero")
+    public  void testNaoEPermitidoRegistrarProdutoComValorIgualaZero(){
+        //Abrir o navegador
+        System.setProperty("webdriver.chrome.driver", "C:\\DTAR\\drivers\\chromedriver_win32\\chromedriver.exe");
+        WebDriver navegador = new ChromeDriver();
+
+        // vou maximizar a tela
+        navegador.manage().window().maximize();
+
+        //vou definir um tempo de espera padrão de 5 segundos
+        navegador.manage().timeouts().implicitlyWait(Duration.ofSeconds(5));
+
+        //Navegar para a pagina da lojinha Web
+        navegador.get("http://165.227.93.41/lojinha-web/v2");
+
+        //Fazer login
+        navegador.findElement(By.cssSelector("label[for= 'usuario']")).click();
+        navegador.findElement(By.id("usuario")).sendKeys("admin");
+        navegador.findElement(By.cssSelector("label[for= 'senha']")).click();
+        navegador.findElement(By.id("senha")).sendKeys("admin");
+        navegador.findElement(By.cssSelector("button[type='submit']")).click();
+
+        //Vou para a tela de registro de produto
+        navegador.findElement(By.linkText("ADICIONAR PRODUTO")).click();
+
+        //Vou preencher dados do produto e o valor sera igual a zero
+        navegador.findElement(By.id("produtonome")).sendKeys("Macbook Pro");
+        navegador.findElement(By.name("produtovalor")).sendKeys("000");
+        navegador.findElement(By.id("produtocores")).sendKeys("preto,branco");
+
+        //Vou submeter o formulário
+        navegador.findElement(By.cssSelector("button[type='submit']")).click();
+
+        //vou validar que a mensagem de erro foi apresentada
+        String mensagemToast = navegador.findElement(By.cssSelector(".toast.rounded")).getText();
+        Assertions.assertEquals("O valor do produto deve estar entre R$ 0,01 e R$ 7.000,00", mensagemToast);
+        navegador.quit();
+
+
+    }
+}
